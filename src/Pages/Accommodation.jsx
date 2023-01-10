a1dcd55af8637198260941d43a301fb37d1bb48b
Maxence <maxence.califano@gmail.com>
2023-01-10 14:51:40 +0200

    create accomodation page

--- src/Pages/Accommodation.jsx
+++ src/Pages/Accommodation.jsx
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Accommodation() {
     //Get accommodation id the url
     let { id } = useParams();
 
+    const [accommodation, setAccommodation] = useState();
+    console.log("🚀 ~ file: Accommodation.jsx:9 ~ Accommodation ~ accommodation", accommodation)
+
+    useEffect(() => {
+        fetch('../logements.json')
+            .then(response => response.json())
+            .then(res => setAccommodation(res.find(elem => elem.id === id)))
+    }, [id])
+
     return (
         <div>
-
+            <p>{accommodation.title}</p>
+            <p>{accommodation.location}</p>
         </div>
     );
 }
--- src/components/Thumb.jsx
+++ src/components/Thumb.jsx
@@ -1,11 +1,11 @@
 import styles from '../styles/thumb.module.css';
 import { Link } from "react-router-dom";
 
-function Thumb(props) {
+function Thumb({ accomodation }) {
     return (
-        <Link to={`/accommodation/${props.accomodation.id}`} className={styles.thumb}>
-            <img src={props.accomodation.cover} alt="le logement" />
-            <p>{props.accomodation.title}</p>
+        <Link to={`/accommodation/${accomodation.id}`} className={styles.thumb}>
+            <img src={accomodation.cover} alt="le logement" />
+            <p>{accomodation.title}</p>
         </Link>
     );
 }
