290986e9162eed29feee435e89fab9a745a54ae4
Maxence <maxence.califano@gmail.com>
2023-01-17 20:16:31 +0200

    carousel's working

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,13 +1,27 @@
 import styles from '../styles/carousel.module.css';
+import { useState } from 'react';
 
 function Carrousel({ pictures }) {
+
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const nextImage = () => setCurrentImage(currentImage === pictures.length - 1 ? 0 : currentImage + 1)
+    const previousImage = () => setCurrentImage(currentImage === 0 ? pictures.length - 1 : currentImage - 1)
+
     return (
-        <div className={styles.container}>
-            {
-                pictures.map((picture, index) =>
-                    <img src={picture} alt="le logement" key={index} />
-                )
-            }
+        <div className={styles.carousel}>
+            <button className={`${styles.arrows} ${styles.leftArrow}`} onClick={previousImage}>Image précédente</button>
+            <button className={`${styles.arrows} ${styles.rightArrow}`} onClick={nextImage}>Image suivante</button>
+            <div className={styles.container}>
+                {
+                    pictures.map((picture, index) =>
+                        index === 0 ?
+                            <img style={{ marginLeft: '-' + currentImage * 100 + '%' }} src={picture} alt="le logement" key={index} />
+                            :
+                            <img src={picture} alt="le logement" key={index} />
+                    )
+                }
+            </div>
         </div>
     );
 }
