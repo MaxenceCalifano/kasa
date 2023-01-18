efa81d243ba54d32eb9bb6f2a5c2817e0a65ad5b
Maxence <maxence.califano@gmail.com>
2023-01-18 19:56:50 +0100

    page about: add text

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,14 +2,16 @@ import Dropdown from "../components/Dropdown";
 
 function About() {
 
-    const exampleText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-
+    const fiabilite = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+    const respect = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    const service = "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+    const securite = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
     return (
         <div style={{ flexGrow: 2 }}>
-            <Dropdown title={"Fiabilité"} text={exampleText} open={false} />
-            <Dropdown title={"Respect"} text={exampleText} open={false} />
-            <Dropdown title={"Service"} text={exampleText} open={false} />
-            <Dropdown title={"Responsabilité"} text={exampleText} open={false} />
+            <Dropdown title={"Fiabilité"} text={fiabilite} open={false} />
+            <Dropdown title={"Respect"} text={respect} open={false} />
+            <Dropdown title={"Service"} text={service} open={false} />
+            <Dropdown title={"Responsabilité"} text={securite} open={false} />
         </div>
     );
 }
